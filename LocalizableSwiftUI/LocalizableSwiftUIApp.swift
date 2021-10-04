655bd0c67d1abc2ad3736f0e87f9726e1aec2927
Maciej Zajda <mac.zajda@gmail.com>
2021-10-04 22:00:36 +0200

    Initial Commit

---
+++ LocalizableSwiftUI/LocalizableSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizableSwiftUIApp.swift
+//  LocalizableSwiftUI
+//
+//  Created by Maciej Zajda on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct LocalizableSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
